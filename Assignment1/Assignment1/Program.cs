ef1e931de412461a6ef434fe05315af319f3c7de
Joseph Daaboul <joseph.jd.daaboul@gmail.com>
2015-02-08 14:03:25 -0500

    started the axiom verifications

--- Assignment1/Assignment1/Program.cs
+++ Assignment1/Assignment1/Program.cs
@@ -9,34 +9,20 @@ namespace Assignment1
     {
         static void Main()
         {
-            var v1 = new Vertex("v1");
-            var v2 = new Vertex("v2");
-            var v3 = new Vertex("v3");
-            var v4 = new Vertex("v4");
-            var v5 = new Vertex("v5");
-            var v6 = new Vertex("v6");
+            //axiom 1
+            var axiom1 = new UndGraph<string>().allVertices.Count;
 
-            var edge1 = new Edge<string>(v1, v2);
-            var edge2 = new Edge<string>(v1, v4);
-            var edge3 = new Edge<string>(v5, v6);
+            //axiom2
+            var axiom2 = new UndGraph<string>().allEdges.Count;
 
-            var test = new UndGraph<string>();
-            var p = 11111;
-            test.addEdge(edge1);
-            test.addEdge(edge2);
-            test.addEdge(edge3);
+            //axiom3
+            var axiom3 = new UndGraph<string>().countAllVertices();
 
-            var vertices = test.vertices();
-            var edges = test.edges();
-            var countVertices = test.countAllVertices();
-            var countEdges = test.countAllEdges();
-            var getEdge = test.getEdge(v2, v1);
-            var incidentEdges = test.incidentEdges(v1);
-            var opposite = test.opposite(v1, edge1);
-            var areAdjacent = test.areAdjacent(v1, v2);
+            //axiom4
+            var axiom4 = new UndGraph<string>().countAllEdges();
 
-
-            
+            //axiom5                 
+                        
         }
     }
 }
